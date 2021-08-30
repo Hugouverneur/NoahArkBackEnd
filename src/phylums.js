45847e8012349c57f321e19fa53a400890aa540a
Hugo <hugo.umbert@estiam.com>
2021-08-30 11:51:16 +0200

    taxinomie

--- src/phylums.js
+++ src/phylums.js
@@ -3,11 +3,11 @@ const express = require('express');
 function createRouter(db) {
     const router = express.Router();
 
-    router.post('/users', (req, res, next) => {
+    router.post('/phylum', (req, res, next) => {
         console.log(req.body);
         db.query(
-            'INSERT INTO users (user_name, user_lastname, user_email) VALUES (?,?,?)', // Requete SQL
-            [req.body.user_name, req.body.user_lastname, req.body.user_email], // Données qui remplaceront les ?. /!\ Bien vérifier que le nom des clés correspond à ce qui est envoyé coté front
+            'INSERT INTO phylum (phylum_name, kingdom_id) VALUES (?,?)', // Requete SQL
+            [req.body.phylum_name, req.body.kingdom_id], // Données qui remplaceront les ?. /!\ Bien vérifier que le nom des clés correspond à ce qui est envoyé coté front
             (error) => {
                 if (error) {
                     console.error(error);
@@ -19,9 +19,9 @@ function createRouter(db) {
         );
     });
 
-    router.get('/users', function (req, res, next) {
+    router.get('/phylum', function (req, res, next) {
         db.query(
-            'SELECT * FROM users ORDER BY user_id',
+            'SELECT * FROM phylum ORDER BY phylum_id',
             [10*(req.params.page || 0)],
             (error, results) => {
                 if (error) {
@@ -34,23 +34,9 @@ function createRouter(db) {
         );
     });
 
-    router.put('/users/:id', function (req, res, next) {
+    router.delete('/phylum/:id', function (req, res, next) {
         db.query(
-            'UPDATE users SET user_name=?, user_lastname=?, user_email=?, WHERE user_id=?',
-            [req.body.user_name, req.body.user_lastname, req.body.user_email, req.body.user_id],
-            (error) => {
-                if (error) {
-                    res.status(500).json({status: 'error'});
-                } else {
-                    res.status(200).json({status: 'ok'});
-                }
-            }
-        );
-    });
-
-    router.delete('/users/:id', function (req, res, next) {
-        db.query(
-            'DELETE FROM users WHERE user_id=?',
+            'DELETE FROM phylum WHERE phylum_id=?',
             [req.params.user_id],
             (error) => {
                 if (error) {
--- src/spicies.js
+++ src/spicies.js
@@ -3,11 +3,11 @@ const express = require('express');
 function createRouter(db) {
     const router = express.Router();
 
-    router.post('/users', (req, res, next) => {
+    router.post('/spicies', (req, res, next) => {
         console.log(req.body);
         db.query(
-            'INSERT INTO users (user_name, user_lastname, user_email) VALUES (?,?,?)', // Requete SQL
-            [req.body.user_name, req.body.user_lastname, req.body.user_email], // Données qui remplaceront les ?. /!\ Bien vérifier que le nom des clés correspond à ce qui est envoyé coté front
+            'INSERT INTO spicies (spicies_name, genus_id) VALUES (?,?)', // Requete SQL
+            [req.body.spicies_name, req.body.genus_id], // Données qui remplaceront les ?. /!\ Bien vérifier que le nom des clés correspond à ce qui est envoyé coté front
             (error) => {
                 if (error) {
                     console.error(error);
@@ -19,9 +19,9 @@ function createRouter(db) {
         );
     });
 
-    router.get('/users', function (req, res, next) {
+    router.get('/spicies', function (req, res, next) {
         db.query(
-            'SELECT * FROM users ORDER BY user_id',
+            'SELECT * FROM spicies ORDER BY spicies_id',
             [10*(req.params.page || 0)],
             (error, results) => {
                 if (error) {
@@ -34,23 +34,9 @@ function createRouter(db) {
         );
     });
 
-    router.put('/users/:id', function (req, res, next) {
+    router.delete('/spicies/:id', function (req, res, next) {
         db.query(
-            'UPDATE users SET user_name=?, user_lastname=?, user_email=?, WHERE user_id=?',
-            [req.body.user_name, req.body.user_lastname, req.body.user_email, req.body.user_id],
-            (error) => {
-                if (error) {
-                    res.status(500).json({status: 'error'});
-                } else {
-                    res.status(200).json({status: 'ok'});
-                }
-            }
-        );
-    });
-
-    router.delete('/users/:id', function (req, res, next) {
-        db.query(
-            'DELETE FROM users WHERE user_id=?',
+            'DELETE FROM spicies WHERE spicies_id=?',
             [req.params.user_id],
             (error) => {
                 if (error) {
